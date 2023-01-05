90a690cd506658509d48fe929c88ae2642772d90
DavidCrisan <davidcrisan13@gmail.com>
2023-01-05 17:23:08 +0200

    Troop Choice

--- README.txt
+++ README.txt
@@ -1,10 +1,10 @@
 # Creating a battle game
 ## The game will have:
-1. There will be 4 sides
-2. Each side will start the game with the same ammount of money
-3. There will be 5 types of unit
-4. The unit will be selected randomly every 5 secconds for 30 seconds
-5. If the side has enough money they can get that unit if not they have to wait 5 seconds to get another chance
+1. There will be 4 sides D
+2. Each side will start the game with the same ammount of money D
+3. There will be 5 types of unit D
+4. The unit will be selected randomly every 1 secconds for 10 seconds
+5. If the side has enough money they can get that unit if not they have to wait 1 seconds to get another chance D
 6. After the end of getting your units the fight starts
 7. Each unit will have certain ammount of damage
 8. The damage of all units will be added
--- main.py
+++ main.py
@@ -1,3 +1,93 @@
-import sys
 import random
-import datetime
+import time
+
+
+
+
+
+orange = {"gold":50,"points":0}
+blue = {"gold":50,"points":0}
+yellow = {"gold":50,"points":0}
+red = {"gold":50,"points":0}
+
+sides = [orange,blue,yellow,red]
+
+round_gold = 50
+
+units = {"soldier":{"cost":5,"damage":5},
+        "spearman":{"cost":10,"damage":20},
+        "shilder":{"cost":20,"damage":40},
+        "archer":{"cost":30,"damage":60},
+        "magician":{"cost":40,"damage":100}
+        }
+
+unit = []
+
+def troops_choice(side):
+
+    troop_damage = []
+    troop_list = []
+    fail_try = []
+    t = 10
+
+    for i in units:
+        unit.append(i)
+    
+    while side["gold"] > 0:
+        if t >= 0:
+            time.sleep(1)
+            t -= 1
+            choices = random.choice(unit)
+            cost = units[choices]["cost"]
+            damage = units[choices]["damage"]
+            if side["gold"] >= cost:
+                side["gold"] -= cost
+                troop_list.append(choices)
+                troop_damage.append(damage)
+                print(f"Troop list: {troop_list}")
+                print(side["gold"])
+                continue
+            else:
+                fail_try.append(choices)
+                print(f"Fail list: {fail_try}")
+                print(side["gold"])
+                continue
+        else:
+            main()
+            break
+    return troop_damage
+
+
+
+
+
+def main():
+    if orange["gold"] > 0:
+        orange_damage = troops_choice(orange)
+    if red["gold"] > 0:
+        red_damage = troops_choice(red)
+    if blue["gold"] > 0:
+        blue_damage = troops_choice(blue)
+    if yellow["gold"] > 0:
+        yellow_damage = troops_choice(yellow)
+
+    return orange_damage,red_damage,blue_damage,yellow_damage
+
+
+orange_damage, red_damage, blue_damage, yellow_damage = main()
+orange_bp = sum(orange_damage)
+print(orange_bp)
+# print(troop_list)
+print(orange["gold"])
+red_bp = sum(red_damage)
+print(red_bp)
+# print(troop_list)
+print(red["gold"])
+blue_bp = sum(blue_damage)
+print(blue_bp)
+# print(troop_list)
+print(blue["gold"])
+yellow_bp = sum(yellow_damage)
+print(yellow_bp)
+# print(troop_list)
+print(yellow["gold"])
